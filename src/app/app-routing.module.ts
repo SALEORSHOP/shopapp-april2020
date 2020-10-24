cd75efb7c3b883fb614daa19c07a3905a620a6c8
SALEORSHOP <telcin2015@gmail.com>
2020-10-24 17:41:58 -0300

    Commit2020v7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AllshopsComponent } from './shop/allshops/allshops.component';
-import { AddnewshopComponent } from './shop/addnewshop/addnewshop.component';
-import { ShopdetailComponent } from './shop/shopdetail/shopdetail.component';
-import { AboutusComponent } from './shop/aboutus/aboutus.component';
-import { ContactusComponent } from './shop/contactus/contactus.component';
-import { HomeComponent } from './shop/home/home.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-
-const routes: Routes = [
-  {
-    path:'',
-    redirectTo:'home',
-    pathMatch:'full'
-  },
-  {
-    path:'home',
-    component:HomeComponent
-  },
-  {
-    path:'allshops',
-    component:AllshopsComponent
-  },
-  {
-    path:'addnewshop',
-    component:AddnewshopComponent
-  },
-  {
-    path:'shopdetail/:shopid',
-    component:ShopdetailComponent
-  },
-  {
-    path:'aboutus',
-    component:AboutusComponent
-  },
-  {
-    path:'contactus',
-    component:ContactusComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './shop/header/header.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {SlideshowComponent} from './shop/slideshow/slideshow.component';
+import {RegistrationComponent} from './shop/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SlideshowComponent,
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
