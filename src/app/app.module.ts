e92db0c0f9969cfbfdb5bc17ca141aa1618e914e
SALEORSHOP <telcin2015@gmail.com>
2020-10-24 10:24:46 -0300

    commit2020v1
    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shop/header/header.component';
-import { SlideshowComponent } from './shop/slideshow/slideshow.component';
-import { RegistrationComponent } from './shop/registration/registration.component';
-import { ShoutoutComponent } from './shop/shoutout/shoutout.component';
-import { AllshopsComponent } from './shop/allshops/allshops.component';
-import { AddnewshopComponent } from './shop/addnewshop/addnewshop.component';
-import { ShopdetailComponent } from './shop/shopdetail/shopdetail.component';
-import { ContactusComponent } from './shop/contactus/contactus.component';
-import { AboutusComponent } from './shop/aboutus/aboutus.component';
-import { HomeComponent } from './shop/home/home.component';
-import { FooterComponent } from './shop/footer/footer.component';
-import { SearchfilterPipe } from './shop/pipe/searchfilter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    SlideshowComponent,
-    RegistrationComponent,
-    ShoutoutComponent,
-    AllshopsComponent,
-    AddnewshopComponent,
-    ShopdetailComponent,
-    ContactusComponent,
-    AboutusComponent,
-    HomeComponent,
-    FooterComponent,
-    SearchfilterPipe
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
