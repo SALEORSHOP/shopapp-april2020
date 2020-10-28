d8007003aa308394bd59ac1805a7ad3a20c79f0e
SALEORSHOP <telcin2015@gmail.com>
2020-10-28 17:48:33 -0300

    Commit2020v17

--- src/app/shop/addnewshop/addnewshop.component.ts
+++ src/app/shop/addnewshop/addnewshop.component.ts
@@ -10,6 +10,15 @@ export class AddnewshopComponent implements OnInit {
   newShop: any = { };
 
   shopName: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  pincode: string;
+  services1: string;
+  services2: string;
+  description: string;
+  shopPicture: string;
 
   constructor() {
   }
@@ -17,5 +26,7 @@ export class AddnewshopComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addNewShop = (value: any) => {  };
+  addNewShop = (value: any) => {
+    console.log(this.newShop);
+  }
 }
